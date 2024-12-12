74a215abe406c404145c5e2beb9c918b0d330d7d
Vinny Meller <vinnymeller@proton.me>
2024-12-11 22:04:43 -0500

    ok it works final update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "avante.nvim nightly flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     avante = {
       url = "github:yetone/avante.nvim";
